3eeb7cd31b8434f4b7832d0f115a23f5ba28a744
EtherealAO <admin@nijigen.cc>
2022-05-30 19:58:40 +0800

    Update

--- src/components/SelectCard.vue
+++ src/components/SelectCard.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
 import type { Card } from '@/interfaces/Card';
-import { inject, ref } from 'vue';
+import { watch, inject, ref, watchEffect } from 'vue';
 const cards = inject<Card[]>('cards')
 const defaultIcon = '/img/icon/chr_icon_0000.png'
+const runningStyles = ['逃', '先', '差', '追']
 
+const filteredCards = ref<Card[]>([])
+const filterRunningStyle = ref<{ [runningStyle: string]: number[]; }>({})
+for (var runningStyle of runningStyles) filterRunningStyle.value[runningStyle] = [0, 0, 0, 0, 0, 0, 0]
 var cardSelection = ref<Card>();
 var categorySelection = ref("all")
 
+watchEffect(() => {
+    if (cards == undefined) return
+    filteredCards.value.length = 0
+    //for (var i in filterRunningStyle.value) {
+    //    for (var j of filterRunningStyle.value[i]) {
+    //        filteredCards.value.push(...cards.filter((element) => element.ProperRunningStyle[i] == j && filteredCards.value.indexOf(element) == -1))
+    //    }
+    //}
+    for (var card of cards) {
+        var pick = true
+        for (var runningStyle in filterRunningStyle.value) {
+            for (var index of filterRunningStyle.value[runningStyle]) {
+                if (filterRunningStyle.value[runningStyle][index - 1] == undefined) continue
+                if (!(filteredCards.value.indexOf(card) == -1 && card.ProperRunningStyle[runningStyle] == filterRunningStyle.value[runningStyle][index - 1])) {
+                    pick = false
+                } else {
+                    console.log(`${runningStyle} ${filterRunningStyle.value[runningStyle][index - 1]}`)
+                }
+            }
+        }
+        if (pick) {
+            filteredCards.value.push(card)
+        }
+    }
+})
 function onCategoryChanged(event: any) {
     categorySelection.value = event.target.value
 }
@@ -15,6 +44,24 @@ function onCardSelected(card: any) {
 }
 function getIconPath(card: Card | undefined) {
     return card == undefined ? defaultIcon : `/img/icon/chr_icon_${card.Id.toString().substring(0, 4)}_${card.RaceDressId}_02.png`
+}
+function getProperText(proper: number) {
+    switch (proper) {
+        case 1:
+            return "G"
+        case 2:
+            return "F"
+        case 3:
+            return "E"
+        case 4:
+            return "D"
+        case 5:
+            return "C"
+        case 6:
+            return "B"
+        case 7:
+            return "A"
+    }
 }
 </script>
 
@@ -55,7 +102,7 @@ function getIconPath(card: Card | undefined) {
                     </div>
                     <div class="modal-body">
                         <div style="text-align:center">
-                            <img id="icon" v-for="card in cards"
+                            <img id="icon" v-for="card in filteredCards"
                                 @click="$emit('update:card', card); onCardSelected(card)" :src="getIconPath(card)"
                                 data-bs-toggle="modal" data-bs-target="#selectCardModal" />
                         </div>
@@ -79,35 +126,20 @@ function getIconPath(card: Card | undefined) {
             </div>
             <div class="offcanvas-body">
                 <div class="container">
-                    按适性：
-                    <div class="dropdown" id="tao">
+                    按适性：<br />
+                    <div v-for="i in runningStyles" class="dropdown dropdown-running-style" :id="i">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-auto-close="outside"
-                            id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
-                            逃
+                            :id="`dropdownMenu${i}`" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{ i }}
                         </button>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                            <li>
+                        <ul class="dropdown-menu" :aria-labelledby="`dropdownMenu${i}`">
+                            <li v-for="j in Array.from(Array(7).keys()).reverse()">
                                 <div class="form-check form-switch">
-                                    <input class="form-check-input" type="checkbox" role="switch" id="A">
-                                    <label class="form-check-label" for="A">A</label>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input" type="checkbox" role="switch" id="B">
-                                    <label class="form-check-label" for="B">B</label>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input" type="checkbox" role="switch" id="C">
-                                    <label class="form-check-label" for="C">C</label>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="form-check form-switch">
-                                    <input class="form-check-input" type="checkbox" role="switch" id="D">
-                                    <label class="form-check-label" for="D">D</label>
+                                    <input class="form-check-input" type="checkbox" role="switch"
+                                        :id="getProperText(j + 1)"
+                                        @change="filterRunningStyle[i][j] = filterRunningStyle[i][j] == 0 ? j + 1 : 0">
+                                    <label class="form-check-label" :for="getProperText(j + 1)">{{ getProperText(j + 1)
+                                    }}</label>
                                 </div>
                             </li>
                         </ul>
@@ -151,6 +183,11 @@ select#categorySelect {
 
 #filterOffcanvas {
     z-index: 1100;
+}
+
+.dropdown.dropdown-running-style {
+    display: inline;
+    margin-left: 20px;
 }
 
 .dropdown-menu,
--- src/interfaces/Card.ts
+++ src/interfaces/Card.ts
@@ -2,14 +2,7 @@ export class Card {
     Id: number = 0;
     Rarity: number = 0;
     RaceDressId: number = 0;
-    ProperDistanceShort: number = 0;
-    ProperDistanceMile: number = 0;
-    ProperDistanceMiddle: number = 0;
-    ProperDistanceLong: number = 0;
-    ProperRunningStyleNige: number = 0;
-    ProperRunningStyleSenko: number = 0;
-    ProperRunningStyleSashi: number = 0;
-    ProperRunningStyleOikomi: number = 0;
-    ProperGroundTurf: number = 0;
-    ProperGroundDirt: number = 0;
+    ProperDistance: { [distance: string]: number; } = {};
+    ProperRunningStyle: { [runningStyle: string]: number; } = {};
+    ProperGround: { [runningStyle: string]: number; } = {};
 }
